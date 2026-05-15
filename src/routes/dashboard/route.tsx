ff01d50513e2891225aa01fd2d66d3e64ae80a43
Ahnaf Abid <ahnafabid.social@gmail.com>
2026-05-15 06:16:02 +0600

    react query added

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -7,6 +7,7 @@ import {
 } from '@/components/ui/sidebar'
 import { AppSidebar } from '#/components/app-sidebar'
 import { getSessionFn } from '#/data/session'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
@@ -19,28 +20,32 @@ export const Route = createFileRoute('/dashboard')({
   },
 })
 
+const dashboardQueryClient = new QueryClient()
+
 function RouteComponent() {
   const { user } = Route.useLoaderData()
 
   return (
-    <div>
-      <SidebarProvider>
-        <AppSidebar user={user} />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
+    <QueryClientProvider client={dashboardQueryClient}>
+      <div>
+        <SidebarProvider>
+          <AppSidebar user={user} />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+              </div>
+            </header>
+            <div className={`flex flex-1 gap-4 p-4 pt-0`}>
+              <Outlet />
             </div>
-          </header>
-          <div className={`flex flex-1 gap-4 p-4 pt-0`}>
-            <Outlet />
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </div>
+    </QueryClientProvider>
   )
 }
